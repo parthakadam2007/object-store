1ad75a35f4f8aa55d9c59b28cddf212f123c278d
Partha Kadam <parthakadam2007@gmail.com>
2026-03-18 23:26:49 +0530

    feat: add exception handler for AccessTokenValidatingException in GlobalExceptionHandler

--- auth_service/src/main/java/com/parthakadam/space/auth_service/exceptions/GlobalExceptionHandler.java
+++ auth_service/src/main/java/com/parthakadam/space/auth_service/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.parthakadam.space.auth_service.exceptions;
 
 
 import com.parthakadam.space.auth_service.exceptions.accessTokenExceptions.AccessTokenInfoSavingException;
+import com.parthakadam.space.auth_service.exceptions.accessTokenExceptions.AccessTokenValidatingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import org.springframework.http.HttpStatus;
@@ -13,13 +14,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessTokenInfoSavingException.class)
-    public ResponseEntity<ErrorResponse> handleAccessException(AccessTokenInfoSavingException ex) {
+    public ResponseEntity<ErrorResponse> handleAccessSavingException(AccessTokenInfoSavingException ex) {
 
         ErrorResponse error = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 ex.getMessage()
         );
 
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+    @ExceptionHandler(AccessTokenValidatingException.class)
+    public ResponseEntity<ErrorResponse> handleAccessValidationException(AccessTokenValidatingException ex) {
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage()
+        );
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
